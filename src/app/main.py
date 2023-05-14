28d0bf4436da3d8a88a61361e851f66368abe813
migregal <devmigregal@gmail.com>
2023-05-14 20:05:20 +0300

    src: init basic ensemble accumulation

--- src/app/main.py
+++ src/app/main.py
@@ -1,20 +1,48 @@
 import argparse
 
-import ray
-
+import cv2
 import matplotlib.pyplot as plt
+
+import ray
 
 from method import Method
 
-def process(path: str, image: str, output: str):
+def process(path: str, input: str, output: str):
+    line_width = None
+    color=(200, 128, 128)
+    # txt_color=(255, 255, 255)
+
     method = Method(path)
+
+    im = cv2.imread(input)
+    lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)
 
     # TODO: add preprocessing
 
-    imgs = method.predict(image)
+    bboxes = method.predict(im)
 
-    for (i, img) in enumerate(imgs):
-        plt.imsave(f'predicted_{i}.png', img)
+    x, y = im.shape[1], im.shape[0]
+    for bbox in bboxes:
+        p1, p2 = (int(x*bbox[0]), int(y*bbox[1])), (int(x*bbox[2]), int(y*bbox[3]))
+        cv2.rectangle(im, p1, p2, color, thickness=lw, lineType=cv2.LINE_AA)
+
+    cv2.imwrite('test.jpeg', im)
+
+
+    # if label:
+    #     tf = max(self.lw - 1, 1)  # font thickness
+    #     w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
+    #     outside = p1[1] - h >= 3
+    #     p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+    #     cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+    #     cv2.putText(self.im,
+    #                 label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+    #                 0,
+    #                 self.lw / 3,
+    #                 txt_color,
+    #                 thickness=tf,
+    #                 lineType=cv2.LINE_AA)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- src/app/method.py
+++ src/app/method.py
@@ -1,13 +1,73 @@
 import os
 
-import ray
+import numpy as np
+
 from ultralytics import YOLO
 
+import ray
+
+
 @ray.remote
-def apply_expert(image: str, expert: YOLO):
+def apply_expert(image: np.ndarray, expert: YOLO) -> np.ndarray:
     r = expert.predict(image)
-    return r[0].plot()
-    # return r[0].boxes.xyxyn
+    # return r[0].plot()
+    return np.array(r[0].boxes.xyxyn)
+
+
+def get_bbox_area(bbox):
+    return (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
+
+
+def get_intersection_coords(bb1, bb2):
+    x_l, y_t = max(bb1[0], bb2[0]), max(bb1[1], bb2[1])
+    x_r, y_b = min(bb1[2], bb2[2]), min(bb1[3], bb2[3])
+
+    return np.array([x_l, y_t, x_r, y_b])
+
+
+def get_iou(bbox_1, bbox_2):
+    x_l, y_t, x_r, y_b = get_intersection_coords(bbox_1, bbox_2)
+    if x_r < x_l or y_b < y_t:
+        return 0.0
+
+    intersection_area = (x_r - x_l) * (y_b - y_t)
+    bbx1_area, bbx2_area = get_bbox_area(bbox_1), get_bbox_area(bbox_2)
+
+    return intersection_area / float(bbx1_area + bbx2_area - intersection_area)
+
+
+def deduplicate_wbboxes(wbboxes, limit=0.75):
+    bboxes = []
+
+    for i in range(len(wbboxes)):
+        cur, skip = wbboxes[i], False
+        for j in range(i + 1, len(wbboxes)):
+            other = wbboxes[j]
+            iou = get_iou(cur[1], other[1])
+            if iou < limit:
+                continue
+
+            if cur[0] < other[0]:
+                skip = True
+                continue
+
+            tmp = get_intersection_coords(cur[1], other[1])
+            if get_bbox_area(tmp) < get_bbox_area(cur[1]):
+                cur[1] = tmp
+
+        if skip:
+            continue
+
+        for j in range(len(bboxes)):
+            bbox = bboxes[j]
+            if limit < get_iou(cur[1], bbox):
+                skip = True
+                break
+
+        if not skip:
+            bboxes += [cur[1]]
+
+    return bboxes
 
 
 class Method:
@@ -28,11 +88,15 @@ class Method:
 
             self.experts += [YOLO(f)]
 
-    def predict(self, input: str):
+    def predict(self, input: np.ndarray):
         ray_ids = []
         for model in self.experts:
             ray_ids += [apply_expert.remote(input, model)]
 
-        ret = ray.get(ray_ids)
-        # print(ret)
-        return ret
+        experts_res = ray.get(ray_ids)
+
+        wbboxes = []
+        for prev in experts_res:
+            wbboxes += [[0.5, bbox] for bbox in prev]
+
+        return deduplicate_wbboxes(wbboxes)
