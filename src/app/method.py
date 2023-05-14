0c33ca2d660e14ceb8bdccb5b9c2ca27c9480fb3
migregal <devmigregal@gmail.com>
2023-05-14 21:35:44 +0300

    src: beautify

--- src/app/method.py
+++ src/app/method.py
@@ -13,29 +13,31 @@ def apply_expert(image: np.ndarray, expert: YOLO) -> np.ndarray:
     return np.array(r[0].boxes.xyxyn)
 
 
-def get_bbox_area(bbox):
+def get_bbox_area(bbox) -> np.float32:
     return (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
 
 
-def get_intersection_coords(bb1, bb2):
-    x_l, y_t = max(bb1[0], bb2[0]), max(bb1[1], bb2[1])
-    x_r, y_b = min(bb1[2], bb2[2]), min(bb1[3], bb2[3])
+def get_intersection_coords(bbox_1: np.ndarray, bbox_2: np.ndarray) -> np.ndarray:
+    x_l, y_t = max(bbox_1[0], bbox_2[0]), max(bbox_1[1], bbox_2[1])
+    x_r, y_b = min(bbox_1[2], bbox_2[2]), min(bbox_1[3], bbox_2[3])
 
     return np.array([x_l, y_t, x_r, y_b])
 
 
-def get_iou(bbox_1, bbox_2):
+def get_iou(bbox_1: list[float], bbox_2) -> np.float32:
     x_l, y_t, x_r, y_b = get_intersection_coords(bbox_1, bbox_2)
     if x_r < x_l or y_b < y_t:
-        return 0.0
+        return np.float32(0.0)
 
     intersection_area = (x_r - x_l) * (y_b - y_t)
     bbx1_area, bbx2_area = get_bbox_area(bbox_1), get_bbox_area(bbox_2)
 
-    return intersection_area / float(bbx1_area + bbx2_area - intersection_area)
+    return np.float32(
+        intersection_area / float(bbx1_area + bbx2_area - intersection_area)
+    )
 
 
-def deduplicate_wbboxes(wbboxes, limit=0.75):
+def deduplicate_wbboxes(wbboxes, limit: float = 0.75) -> np.ndarray:
     bboxes = []
 
     for i in range(len(wbboxes)):
@@ -66,7 +68,7 @@ def deduplicate_wbboxes(wbboxes, limit=0.75):
         if not skip:
             bboxes += [cur[1]]
 
-    return bboxes
+    return np.array(bboxes)
 
 
 class Method:
@@ -87,7 +89,7 @@ class Method:
 
             self.experts += [YOLO(f)]
 
-    def predict(self, input: np.ndarray):
+    def predict(self, input: np.ndarray) -> np.ndarray:
         ray_ids = []
         for model in self.experts:
             ray_ids += [apply_expert.remote(input, model)]
