6857194aa879abe145c00f0751a5a422a8cf8fff
migregal <devmigregal@gmail.com>
2023-05-15 21:15:40 +0300

    src: fix remote execution

--- src/app/method/detect.py
+++ src/app/method/detect.py
@@ -1,5 +1,6 @@
 import os
 
+import cv2
 import numpy as np
 from ultralytics import YOLO
 import ray
@@ -28,8 +29,9 @@ class Detect:
 
         self.experts += [YOLO(f) for f in fnames if os.path.isfile(f)]
 
-    def predict(self, input: np.ndarray) -> np.ndarray:
-        ray_ids = [apply_expert.remote(input, model) for model in self.experts]
+    def predict(self, input: cv2.Mat) -> np.ndarray:
+        input_id = ray.put(input)
+        ray_ids = [apply_expert.remote(input_id, model) for model in self.experts]
         r = ray.get(ray_ids)
 
         wbboxes = [[0.5, np.array(b), np.float32(c)] for p in r for b, c in zip(*p)]
